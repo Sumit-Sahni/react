595d7783ba8e3ed72b80aade448bbac0490cd661
Sumit Sahni <sumit.123sahni@gmail.com>
2021-10-23 11:41:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Landingpage from './Landing/landingpage.js'
 import Links from './Links/Links';
-import { Route, Router, Switch} from 'react-router-dom';
+import { Route} from 'react-router-dom';
 import Home from "./components/Home";
 import Navbar from './components/Navbar.js';
 import Nasaphoto from "./components/Nasaphoto";
 import MovieComponents from "./MovieApi/MovieComponents"
 
 function App() {
+
   return (
     <div>
-      <Router  basename={process.env.PUBLIC_URL }>
-        <Switch>
+
        <Route exact path="/Navbar" component={Navbar} />
        <Route exact path="/Nasaphoto" component={Nasaphoto} />
-       <Route exact path="https://sumit-sahni.github.io/react/" component={Landingpage} />
-       <Route exact path="/https://sumit-sahni.github.io/react/" component={Links} />
-       <Route exact path="/https://sumit-sahni.github.io/react/" component={Home} />
+       <Route exact path="/" component={Landingpage} />
+       <Route exact path="/" component={Links} />
+       <Route exact path="/" component={Home} />
        <Route exact path="/MovieApis" component={MovieComponents} />
-       </Switch>
-     </Router>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-      <App />
+     
+    <Provider store={store}>
+    <Router basename={process.env.PUBLIC_URL}>< App /></Router>,
       </Provider>
- 
-    </BrowserRouter>
-
   </React.StrictMode>,
   document.getElementById('root')
 );
